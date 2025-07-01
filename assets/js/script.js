b65b397ae5f6a9a8547b5a96cefffbe1fd4559f6
AndyV773 <ajv103@hotmail.com>
2025-07-01 11:27:19 +0100

    style: qr code generator

---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+const input = document.getElementById('input');
+const qrcodeContainer = document.getElementById('qrcode');
+const qrColor = document.getElementById('qrColor');
+const bgColor = document.getElementById('bgColor');
+const qrSize = document.getElementById('qrSize');
+const mode = document.getElementById('mode');
+const modeLabel = document.getElementById('mode-label')
+const downloadBtn = document.getElementById('downloadBtn');
+
+function generateQRCode() {
+    const text = input.value.trim() || 'Hello';
+    const size = parseInt(qrSize.value);
+    const fg = qrColor.value;
+    const bg = bgColor.value;
+    qrcodeContainer.innerHTML = '';
+
+    // Use QRCode.js
+    QRCode.toCanvas(text, {
+      width: size,
+      margin: 2,
+      color: {
+        dark: fg,
+        light: bg
+      }
+    }, function (err, canvas) {
+      if (err) return console.error(err);
+      qrcodeContainer.appendChild(canvas);
+    });
+}
+
+function downloadQRCode() {
+    const canvas = qrcodeContainer.querySelector('canvas');
+    const size = parseInt(qrSize.value);
+    const fg = qrColor.value;
+    const bg = bgColor.value;
+
+    if (canvas) {
+        const link = document.createElement('a');
+        link.href = canvas.toDataURL('image/png');
+        link.download = `${size}${fg}${bg}.png`;
+        link.click();
+    }
+}
+
+function toggleMode() {
+  document.body.classList.toggle('dark-mode', mode.checked);
+  
+  if (mode.checked) {
+        modeLabel.innerHTML = `<i class="fa-regular fa-sun"></i>`
+    } else {
+        modeLabel.innerHTML = `<i class="fa-regular fa-moon"></i>`
+    }
+}
+
+// Events
+[input, qrColor, bgColor, qrSize].forEach(el =>
+  el.addEventListener('input', generateQRCode)
+);
+mode.addEventListener('change', toggleMode);
+downloadBtn.addEventListener('click', downloadQRCode);
+
+// function call
+generateQRCode();
+toggleMode();
